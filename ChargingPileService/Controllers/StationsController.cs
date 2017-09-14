f4402cb504f404ea9c376f45b44bb24d7176b995
imapolaris <imapolaris@github.com>
2017-09-14 16:23:36 +0800

    add station service

---
+++ ChargingPileService/Controllers/StationsController.cs
@@ -0,0 +1,57 @@
+﻿using ChargingPileService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ChargingPileService.Controllers
+{
+    public class StationsController : ApiController
+    {
+        Station[] stations = new Station[]
+        {
+            new Station
+            {
+                Id="1",
+                Name="加速器一区充电站",
+                Longitude=116.2499720000,
+                Latitude=40.0885740000,
+                Numbers="1/2",
+                Address="北京市海淀区永丰产业基地加速器一区充电站",
+            },
+            new Station
+            {
+                Id="2",
+                Name="滕州站充电站",
+                Longitude=117.263977,
+                Latitude=35.098302,
+                Numbers="3/4",
+                Address="山东省枣庄市滕州站",
+            },
+            new Station
+            {
+                Id="3",
+                Name="徐汇区充电站",
+                Longitude=121.44317,
+                Latitude=31.19541,
+                Numbers="0/5",
+                Address="上海徐汇区徐家汇",
+            },
+        };
+
+        public IEnumerable<Station> GetAllStations()
+        {
+            return stations;
+        }
+
+        public IHttpActionResult GetStation(string id)
+        {
+            var station = stations.FirstOrDefault(_ => _.Id == id);
+            if (station == null)
+                return NotFound();
+            return Ok(station);
+        }
+    }
+}
---
+++ ChargingPileService/Models/Station.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChargingPileService.Models
+{
+    public class Station
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public string Numbers { get; set; }
+        public string Address { get; set; }
+    }
+}
